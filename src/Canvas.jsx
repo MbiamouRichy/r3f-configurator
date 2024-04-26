9174b79845ef09ae4d7ca44062d26a6db4b8247c
MbiamouRichy <richymbiamou52@gmail.com>
2024-04-26 19:03:38 +0100

    update et fin du projet

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,3 +1,7 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useRef } from "react";
+
 import {
   AccumulativeShadows,
   Center,
@@ -7,27 +11,24 @@ import {
   useGLTF,
   useTexture,
 } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { easing } from "maath";
-import { useRef } from "react";
 import { useSnapshot } from "valtio";
 import { state } from "./store";
-let position = [-1, 0, 2.5];
-let fov = 25;
 
-export const App = () => (
+export const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
   <Canvas
     shadows
+    gl={{ preserveDrawingBuffer: true }}
+    camera={{ position, fov }}
     eventSource={document.getElementById("root")}
     eventPrefix="client"
-    camera={{ position, fov }}
   >
     <ambientLight intensity={0.5} />
-    <Environment preset="city" />
+    <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
+
     <CameraRig>
+      <Backdrop />
       <Center>
         <Shirt />
-        <Backdrop />
       </Center>
     </CameraRig>
   </Canvas>
@@ -43,12 +44,13 @@ function Shirt(props) {
   useFrame((state, delta) =>
     easing.dampC(materials.lambert1.color, snap.selectedColor, 0.25, delta)
   );
+
   return (
     <mesh
       castShadow
-      receiveShadow
       geometry={nodes.T_Shirt_male.geometry}
       material={materials.lambert1}
+      material-roughness={1}
       {...props}
       dispose={null}
     >
@@ -64,8 +66,20 @@ function Shirt(props) {
 }
 
 function Backdrop() {
+  const shadows = useRef();
+
+  useFrame((state, delta) =>
+    easing.dampC(
+      shadows.current.getMesh().material.color,
+      state.selectedColor,
+      0.25,
+      delta
+    )
+  );
+
   return (
     <AccumulativeShadows
+      ref={shadows}
       temporal
       frames={60}
       alphaTest={0.85}
@@ -76,15 +90,15 @@ function Backdrop() {
       <RandomizedLight
         amount={4}
         radius={9}
-        intensity={2.55}
-        ambient={0.55}
+        intensity={0.55}
+        ambient={0.25}
         position={[5, 5, -10]}
       />
       <RandomizedLight
         amount={4}
         radius={5}
         intensity={0.25}
-        ambient={2.55}
+        ambient={0.55}
         position={[-5, 5, -9]}
       />
     </AccumulativeShadows>
@@ -93,7 +107,16 @@ function Backdrop() {
 
 function CameraRig({ children }) {
   const group = useRef();
+
+  const snap = useSnapshot(state);
+
   useFrame((state, delta) => {
+    easing.damp3(
+      state.camera.position,
+      [snap.intro ? -state.viewport.width / 4 : 0, 0, 2],
+      0.25,
+      delta
+    );
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
@@ -101,8 +124,8 @@ function CameraRig({ children }) {
       delta
     );
   });
-
   return <group ref={group}>{children}</group>;
 }
 
 useGLTF.preload("/shirt_baked_collapsed.glb");
+["/react.png", "/three2.png", "/pmndrs.png"].forEach(useTexture.preload);
