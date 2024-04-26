6825b9dfaf57799b2e75688917821edceb478e8b
MbiamouRichy <richymbiamou52@gmail.com>
2024-04-26 19:09:23 +0100

    fin du projet

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
 import { Canvas, useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 import { useRef } from "react";
